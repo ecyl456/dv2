4094aaa5c294e55fed88d45ab36ee8b9199e7286
ecyl456 <eche0083@student.monash.edu>
2024-10-07 22:55:02 +0800

    datasets and base files

---
+++ vegalite.js
@@ -0,0 +1,23 @@
+
+var vg_1 = "propSym.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#propSym", vg_1)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+    .then(function (result) {
+        // You can access the Vega view instance as result.view
+    })
+    .catch(console.error);
+
+var vg_2 = "heatmap.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#heatmap", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+    .then(function (result) {
+        // You can access the Vega view instance as result.view
+    })
+    .catch(console.error);
+var vg_3 = "choropleth.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#choropleth", vg_3)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+    .then(function (result) {
+        // You can access the Vega view instance as result.view
+    })
+    .catch(console.error);
