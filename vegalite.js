e3b0c1f9a568a5d474eccf5f6b6a6b08564e3a48
ecyl456 <eche0083@student.monash.edu>
2024-10-08 19:19:25 +0800

    finished choropleth and proportional symbol map, and new datasets

--- vegalite.js
+++ vegalite.js
@@ -7,13 +7,23 @@ vegaEmbed("#propSym", vg_1)  // Embeds the chart into an HTML element with the I
     })
     .catch(console.error);
 
-var vg_2 = "heatmap.json";  // Path to your Vega-Lite JSON file
+// var vg_2 = "heatmap.json";  // Path to your Vega-Lite JSON file
 
-vegaEmbed("#heatmap", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+// vegaEmbed("#heatmap", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+//     .then(function (result) {
+//         // You can access the Vega view instance as result.view
+//     })
+//     .catch(console.error);
+
+var vg_2 = "parallelCo.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#parallel", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
     .then(function (result) {
         // You can access the Vega view instance as result.view
     })
     .catch(console.error);
+
+
 var vg_3 = "choropleth.json";  // Path to your Vega-Lite JSON file
 
 vegaEmbed("#choropleth", vg_3)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
