6df2a14f2e0b3f0c2abe02b469641e43f10d004a
ecyl456 <eche0083@student.monash.edu>
2024-10-10 14:21:39 +0800

    Updated charts

--- vegalite.js
+++ vegalite.js
@@ -7,17 +7,9 @@ vegaEmbed("#propSym", vg_1)  // Embeds the chart into an HTML element with the I
     })
     .catch(console.error);
 
-// var vg_2 = "heatmap.json";  // Path to your Vega-Lite JSON file
+var vg_2 = "donut.json";  // Path to your Vega-Lite JSON file
 
-// vegaEmbed("#heatmap", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
-//     .then(function (result) {
-//         // You can access the Vega view instance as result.view
-//     })
-//     .catch(console.error);
-
-var vg_2 = "parallelCo.json";  // Path to your Vega-Lite JSON file
-
-vegaEmbed("#parallel", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+vegaEmbed("#donut", vg_2)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
     .then(function (result) {
         // You can access the Vega view instance as result.view
     })
@@ -27,6 +19,24 @@ vegaEmbed("#parallel", vg_2)  // Embeds the chart into an HTML element with the 
 var vg_3 = "choropleth.json";  // Path to your Vega-Lite JSON file
 
 vegaEmbed("#choropleth", vg_3)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+    .then(function (result) {
+        // You can access the Vega view instance as result.view
+    })
+    .catch(console.error);
+
+
+var vg_4 = "stackBar.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#stackBar", vg_4)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
+    .then(function (result) {
+        // You can access the Vega view instance as result.view
+    })
+    .catch(console.error);
+
+
+var vg_5 = "bar.json";  // Path to your Vega-Lite JSON file
+
+vegaEmbed("#bar", vg_5)  // Embeds the chart into an HTML element with the ID 'choropleth_map'
     .then(function (result) {
         // You can access the Vega view instance as result.view
     })
